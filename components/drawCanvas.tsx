8b2bdf1f6e770cbb48b3f46af59558189aad08a9
takonasu <piano88_viola_tako8@hotmail.co.jp>
2022-05-27 16:12:39 +0900

    add: 縁取り

--- components/drawCanvas.tsx
+++ components/drawCanvas.tsx
@@ -7,6 +7,9 @@ type Props = {
 const DrawCanvas: React.FC<Props> = ({ text }) => {
 	const width = 600;
 	const height = 255;
+	const strolkeSize = 1.5;
+	const textSize = 110;
+
 	const [png, setPng] = useState<string | null>(null);
 	useEffect(() => {
 		const canvasElem = document.createElement('canvas');
@@ -20,14 +23,16 @@ const DrawCanvas: React.FC<Props> = ({ text }) => {
 		ctx.clearRect(0, 0, width, height);
 		// ctx.fillStyle = 'rgba(' + [0, 0, 0, 0] + ')';
 		ctx.fillRect(0, 0, width, height);
-		ctx.font = '60px BasiliskFont';
+		ctx.font = `${textSize}px BasiliskFont`;
 
 		const grad = ctx.createLinearGradient(50, 80, 50, 120);
 		grad.addColorStop(1.0, '#E60AF9');
 		grad.addColorStop(0.0, '#550082');
-
+		ctx.lineWidth = strolkeSize;
+		ctx.strokeStyle = '#fdf979';
 		ctx.fillStyle = grad;
 		ctx.fillText(text, 0, height / 2);
+		ctx.strokeText(text, 0, height / 2);
 
 		setPng(canvasElem.toDataURL());
 	}, [text]);
