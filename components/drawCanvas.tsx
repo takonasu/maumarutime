319ca82e595e41d77aa99aaac0752681e75239f4
takonasu <piano88_viola_tako8@hotmail.co.jp>
2022-06-01 13:54:33 +0900

    fix: うまくレンダリングされないバグ修正

--- components/drawCanvas.tsx
+++ components/drawCanvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { text } from 'stream/consumers';
 import styles from '../styles/DrawField.module.css';
 
@@ -7,10 +7,17 @@ type Props = {
 	lowerText: string;
 };
 
+const imageWidth = 1500;
+const imageHeight = 400;
+const fontSize = 200;
+
 const makeTextImage = (text: string): string | null => {
-	const textSize = 200;
-	const width = 1500;
-	const height = 200;
+	if (typeof window !== 'object') {
+		return null;
+	}
+	const textSize = fontSize;
+	const width = imageWidth;
+	const height = imageHeight / 2;
 	const strolkeSize = 1.5;
 	const canvasElem = document.createElement('canvas');
 	canvasElem.width = width;
@@ -38,20 +45,28 @@ const makeTextImage = (text: string): string | null => {
 
 const DrawCanvas: React.FC<Props> = ({ upperText, lowerText }) => {
 	const [png, setPng] = useState<string | null>(null);
+	const upperImageSrc = useMemo(() => makeTextImage(upperText) ?? '', [upperText]);
+	const lowerImageSrc = useMemo(() => makeTextImage(lowerText) ?? '', [lowerText]);
+
 	useEffect(() => {
 		const upperImage = new Image();
 		const lowerImage = new Image();
-		upperImage.src = makeTextImage(upperText) ?? '';
-		lowerImage.src = makeTextImage(lowerText) ?? '';
+		upperImage.src = upperImageSrc;
+		lowerImage.src = lowerImageSrc;
 		const combinedCanvasElm = document.createElement('canvas');
 		const ctx = combinedCanvasElm && combinedCanvasElm.getContext('2d');
 		if (!combinedCanvasElm || !ctx) return;
 
-		combinedCanvasElm.height = 400;
-		combinedCanvasElm.width = 1500;
-		ctx.drawImage(upperImage, 0, 0);
-		ctx.drawImage(lowerImage, 0, 200);
-		setPng(combinedCanvasElm.toDataURL());
+		combinedCanvasElm.width = imageWidth;
+		combinedCanvasElm.height = imageHeight;
+		upperImage.onload = () => {
+			ctx.drawImage(upperImage, 0, 0);
+			setPng(combinedCanvasElm.toDataURL());
+		};
+		lowerImage.onload = () => {
+			ctx.drawImage(lowerImage, 0, 200);
+			setPng(combinedCanvasElm.toDataURL());
+		};
 	}, [upperText, lowerText]);
 	return (
 		<div className={styles.card}>
